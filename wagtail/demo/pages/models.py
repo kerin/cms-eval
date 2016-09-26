19fbffd583ad344c5a3889499a2de0cddf78bd1e
Kerin Cosford <cosford@gmail.com>
2016-09-26 16:40:21 +0100

    Added pages app

---
+++ wagtail/demo/pages/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+
+from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.fields import StreamField
+from wagtail.wagtailcore import blocks
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.wagtailimages.blocks import ImageChooserBlock
+
+
+class SimplePage(Page):
+    # title = models.CharField(max_length=255)
+    intro = models.CharField(max_length=255)
+    date = models.DateField("Post date")
+
+    body = StreamField([
+        ('callout', blocks.StructBlock([
+            ('title', blocks.CharBlock()),
+            ('paragraph', blocks.RichTextBlock()),
+        ])),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+
+        ('section', blocks.StructBlock([
+            ('title', blocks.CharBlock()),
+            ('subsections', blocks.ListBlock(
+                blocks.StructBlock([
+                    ('title', blocks.CharBlock()),
+                    ('image', ImageChooserBlock()),
+                    ('body', blocks.RichTextBlock())
+                ])
+            ))
+        ])),
+
+        # ('section', blocks.ListBlock(
+        #     blocks.StructBlock([
+        #         ('title', blocks.CharBlock()),
+        #         ('image', ImageChooserBlock()),
+        #         ('body', blocks.RichTextBlock())
+        #     ], label="Subsection")
+        # ))
+    ])
+
+    aside = StreamField([
+        ('alert', blocks.RichTextBlock()),
+        ('paragraph', blocks.RichTextBlock()),
+    ])
+
+    content_panels = Page.content_panels + [
+        # FieldPanel('title'),
+        FieldPanel('intro'),
+        FieldPanel('date'),
+        StreamFieldPanel('body'),
+        StreamFieldPanel('aside')
+    ]
